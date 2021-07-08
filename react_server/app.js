826ea46ba061fc1ba02605dd170982f08c22e9f6
hyunw3 <0176778602@naver.com>
2021-07-08 14:22:02 +0000

    person DB changes to titles.js (title DB)

--- react_server/app.js
+++ react_server/app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var titleRouter = require('./routes/titles');
 
 var app = express();
 
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../react-client/build')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/titles', titleRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
