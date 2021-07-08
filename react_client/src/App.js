951033ddd606a2e4a5455eca88b2916f2f5c71ad
hyunw3 <0176778602@naver.com>
2021-07-08 14:21:05 +0000

    black-nav add

--- react_client/src/App.js
+++ react_client/src/App.js
@@ -8,19 +8,28 @@ class App extends Component {
   state = {users: []}
 
   componentDidMount() {
-    fetch('/users')
+    fetch('/titles')
       .then(res => res.json())
       .then(users => this.setState({users}))
   };
   render() {
     return (
       <div className="App">
-        <h1>Users</h1>
-        {this.state.users.map(user =>
-          <div key={user.id}>
-            {user.name}
-          </div>
-        )}
+		<div className="black-nav">
+			<h2> 개발 Blog </h2>	
+		</div>
+		<div className="contents">
+        	<h1>Users</h1>
+			
+        	{this.state.users.map(user =>
+          		<div key={user.id}>
+            		{user.title} <br/>
+					{user.date}에 작성
+					<hr/>
+          		</div>
+        	)}
+		
+		</div>
       </div>
     );
   }
