ebb666aebc70bd81b2a5e603ae4fde79aa60992f
hyunw3 <0176778602@naver.com>
2021-07-14 00:51:37 +0000

    thumbs index added

--- react_server/routes/titles.js
+++ react_server/routes/titles.js
@@ -4,8 +4,8 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.json([
-    {id: 1, title: "정인철 배고픔", date :"2019-07-08"},
-    {id: 2, title: "정인철 밥먹음", date :"2019-07-12"}
+    {id: 1, title: "정인철 배고픔", date :"2019-07-08",thumbs : 0},
+    {id: 2, title: "정인철 밥먹음", date :"2019-07-12",thumbs : 0}
   ]);
   //res.send('respond with a resource');
 });
