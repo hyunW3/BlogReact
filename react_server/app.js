76e82c3350807a73c21e3f54e0e15ec47dfb3933
hyunw3 <0176778602@naver.com>
2021-07-14 04:27:14 +0000

    mongoose is installed

--- react_server/app.js
+++ react_server/app.js
@@ -1,12 +1,13 @@
 var createError = require('http-errors');
 var express = require('express');
+const mongoose = require('mongoose')
+const db_info = require('./config/mongodb_info.js')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var titleRouter = require('./routes/titles');
-
 var app = express();
 
 // view engine setup
@@ -19,8 +20,30 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../react-client/build')));
 
+//request mapping
 app.use('/', indexRouter);
 app.use('/titles', titleRouter);
+// db connect
+mongoose.connect(db_info.MongoURI,{
+	useNewUrlParser: true, useUnifiedTopology: true
+}).then(() => console.log("MongoDB connected... "))
+.catch(error => console.log(error))
+
+
+
+const tmp_data = new db_info.Title_DB({
+	id : 3,
+	title : "정인철 밥먹음",
+	date : new Date("<2019-07-14>"),
+	thumbs : 3,
+});
+tmp_data.save()
+	.then(() => {
+	console.log(tmp_data);
+})
+	.catch((err) => {
+	console.log("Error :" + err)
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
