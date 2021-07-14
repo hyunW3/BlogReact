332ebadf7fead655c14ae2dd1e533d31c50cf99f
hyunw3 <0176778602@naver.com>
2021-07-14 07:49:46 +0000

    prettify component disqus.js

--- react_client/src/components/disqus.js
+++ react_client/src/components/disqus.js
@@ -1,19 +1,20 @@
 import React, {Component} from 'react';
 import {DiscussionEmbed} from 'disqus-react';
-class Disqus_comment extends Component{
+class DisqusComment extends Component{
 	render(){
 		const disqusShortname = "react-blog-4"
-    const disqusConfig = {
-      	url: "https://react-ohbae-bzomu.run.goorm.io",
-      	identifier: "article-id",
-      	title: "Title of Your Article"
-	}
+    	const disqusConfig = {
+			url: "https://react-ohbae-bzomu.run.goorm.io",
+      		identifier: "article-id",
+      		title: "Title of Your Article"
+		}
 		return(
-			<DiscussionEmbed style={{padding : "20px"}}
-    	shortname={disqusShortname}
-    	config={disqusConfig  }
-	/>
+			<DiscussionEmbed 
+				style={{padding : "20px"}}
+    			shortname={disqusShortname}
+    			config={disqusConfig  }
+			/>
 		)
 	}
 }
-export default Disqus_comment;
+export default DisqusComment;
