ca32e7b2552f7052417e226f9cb08d83cf321ece
Seungjun Oh <47017729+sjuuun@users.noreply.github.com>
2021-09-28 23:24:14 +0900

    apply linter to server code (#13)
    * apply code style
    
    * style: linter 적용사항 반영

--- react_server/app.js
+++ react_server/app.js
@@ -1,53 +1,21 @@
-var createError = require('http-errors');
-var express = require('express');
-const mongoose = require('mongoose'); 
-const db_info = require('./config/mongodb_info.js');
-const contents = require('./schemas/contents')
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const mongoose = require('mongoose');
+const dbInfo = require('./config/mongodb_info.json');
 
-var contentSchema = require('./schemas/contents')
+const contentRouter = require('./routes/content');
 
-var contentRouter = require('./routes/content');
-var app = express(); // TODO : asyncify
+const app = express(); // TODO : asyncify
 
-
-// view engine setup
-app.set('views', path.join(__dirname, '../react-client/build'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../react-client/build')));
 
-//request mapping
-app.use('/contents',contentRouter);
+// request mapping
+app.use('/contents', contentRouter);
+
 // db connect
-mongoose.connect(db_info.MongoURI,{
-	useNewUrlParser: true, useUnifiedTopology: true
-}).then(() => console.log("MongoDB connected... "))
-.catch(error => console.log(error))
-
-
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+mongoose.connect(dbInfo.MongoURI, {
+  useNewUrlParser: true, useUnifiedTopology: true,
+}).then(() => console.log('MongoDB connected... '))
+  .catch((error) => console.log(error));
 
 module.exports = app;
--- react_server/schemas/contents.js
+++ react_server/schemas/contents.js
@@ -1,32 +1,27 @@
-const mongoose = require('mongoose')
-const contentSchema = new mongoose.Schema({
-	id : {
-		type : Number
-	},
-	title : {
-		type : String,
-		require : true,
-		trim : true,
-	},
-	content : {
-		type : String,
-		require : true
-	},
-	date : {
-		type : Date,
-		default : Date.now,
-	},
-	thumbs : {
-		type : Number,
-		validate(value) {
-			if(value <0) {
-				value = 0;
-			}
-		}
-	}
-})
+const mongoose = require('mongoose');
 
+const contentSchema = new mongoose.Schema({
+  id: {
+    type: Number,
+  },
+  title: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  content: {
+    type: String,
+    require: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  thumbs: {
+    type: Number,
+  },
+});
 
 module.exports = {
-	contentSchema : mongoose.model("contents",contentSchema),
-}
+  contentSchema: mongoose.model('contents', contentSchema),
+};
