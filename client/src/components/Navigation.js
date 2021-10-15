b6307e0782494b42f04fa560593aa5da57c856fb
hyunw3 <0176778602@naver.com>
2021-10-15 06:55:26 +0000

    refactor: css file classification & arrangement

---
+++ client/src/components/Navigation.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../css/Navigation.css";
+
+const Navigation = () => {
+  return (
+    <div className="black-nav">
+      <h2> 개발 Blog </h2>
+    </div>
+  );
+};
+export default Navigation;
