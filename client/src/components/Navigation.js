c97cfb089d784f226f6458cc517e6c54e36bd1db
hyunw3 <0176778602@naver.com>
2021-10-15 13:50:16 +0000

    feat(client): 네비게이션바의 정보를 불러오고 보여주는 기능을 구현했습니다. (DB에서 카테고리 정보를 가지고 있어요)

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import "../css/Navigation.css";
 
 const Navigation = () => {
+  const [collectionList, setCollectionList] = useState([]);
+  useEffect(() => {
+    const newArr = [];
+    fetch("./collectionList")
+      .then((res) => res.json())
+      .then((res) => res.forEach((data) => newArr.push(data.name)))
+      .then((res) => {
+        setCollectionList(newArr);
+      });
+  }, []);
+
   return (
     <div className="black-nav">
-      <h2> 개발 Blog </h2>
+      <h2 className="nav-title"> 개발 Blog </h2>
+      <nav className="Category">
+        {collectionList.map((data) => (
+          <li className="Category-item" key={data}>
+            {data}{" "}
+          </li>
+        ))}
+      </nav>
     </div>
   );
 };
