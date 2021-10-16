8241b92fef672e453ce131971b42dae6cc7654de
hyunw3 <0176778602@naver.com>
2021-10-16 06:38:27 +0000

    fix(client): navigation : route to request of categories changed(collectionlist -> categories)

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -3,30 +3,26 @@ import { Link } from "react-router-dom";
 import "../css/Navigation.css";
 
 const Navigation = () => {
-  const [collectionList, setCollectionList] = useState([]);
+  const [categories, setCategories] = useState([]);
   useEffect(() => {
     const newArr = [];
-    fetch("./collectionList")
+    fetch("./categories")
       .then((res) => res.json())
       .then((res) => res.forEach((data) => newArr.push([data._id, data.name])))
       .then(() => {
-        setCollectionList(newArr);
+        setCategories(newArr);
       });
   }, []);
 
   return (
     <div className="black-nav">
       <h2 className="nav-title"> 개발 Blog </h2>
-      <nav className="Category">
-        {collectionList.map(([_id, name]) => (
-          <>
-            <Link to={name}>
-              <li className="Category-item" key={_id}>
-                {name}{" "}
-              </li>
-            </Link>
+      <nav className="category">
+        {categories.map(([id, name]) => (
+          <Link to={name} key={id}>
+            <li className="Category-item">{name} </li>
             <li>|</li>
-          </>
+          </Link>
         ))}
       </nav>
     </div>
